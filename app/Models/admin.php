7561420189895dbe441d8920b8e9804ffe56805f
Tempebenguk <erricksb592@gmail.com>
2024-08-07 15:50:41 +0700

    foto

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class admin extends Model
 {
@@ -27,4 +28,16 @@ class admin extends Model
         'jenkel',
         'status',
     ];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function foto(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($foto) => url('/storage/admin/' . $foto),
+        );
+    }
 }
--- app/Models/pegawai.php
+++ app/Models/pegawai.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class pegawai extends Model
 {
@@ -31,4 +32,11 @@ class pegawai extends Model
         'jenkel',
         'status',
     ];
+
+    protected function foto(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($foto) => url('/storage/pegawai/' . $foto),
+        );
+    }
 }
